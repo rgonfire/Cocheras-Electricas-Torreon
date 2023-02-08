4488be8616a49c8bff0a8dd914043d493861eb43
Raul Galindo <raulgasa1997@gmail.com>
2023-02-08 14:16:24 -0600

    Se agrego google tag al sitio web.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/index.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.REACT_APP_GTM_ID });
+  }, []);
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
