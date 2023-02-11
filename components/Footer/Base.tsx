f1d4b445d4e9e6b7d345fac667f3120e01aba849
Raul Galindo <raulgasa1997@gmail.com>
2023-02-11 12:08:34 -0600

    Resolvi problemas responsivos con el footer.

--- components/Footer/Base.tsx
+++ components/Footer/Base.tsx
@@ -4,27 +4,25 @@
 
 const Index = (): JSX.Element => {
   return (
-    <footer className="py-8 bg-black border border-white">
-      <div className=" h-30 gap-x-10 text-white flex flex-col md:flex-row justify-center items-center">
-        <div className="px-10 xl:w-2/5">
-          <h4>Cocheras Electricas Torreon</h4>
-        </div>
-
-        <div className="px-10 xl:w-3/5">
+    <footer className="py-10 bg-black">
+      <div className="px-10 gap-x-10 text-white md:gap-y-2 flex flex-col flex-wrap md:flex-row justify-left items-left">
+        <div>
           <h3>Horario:</h3>
           <p className="underline">Lunes a sabado de 9:00 - 19:00</p>
         </div>
-      </div>
 
-      <div className="h-30 gap-x-10 text-white flex flex-col md:flex-row justify-center">
-        <div className="px-10 xl:w-2/5">
+        <div>
           <h3>Teléfono:</h3>
           <p className="underline">8711413457</p>
         </div>
 
-        <div className="pt-1 px-10 xl:w-3/5">
-          <h3>Direccion:</h3>
+        <div>
+          <h3>Dirección:</h3>
           <p className="underline">Calle Rio sena 1357, Magdalenas</p>
+        </div>
+
+        <div>
+          <h4>Cocheras Electricas Torreon</h4>
         </div>
       </div>
     </footer>
--- components/Hero/Base.tsx
+++ components/Hero/Base.tsx
@@ -3,7 +3,7 @@ import { TypeHero } from "components/types/hero.types";
 const Index = ({ title, text }: TypeHero): JSX.Element => {
   return (
     <div className="bg-black h-screen">
-      <div className="text-white h-screen px-4 xl:pl-24 xl:pr-56 flex flex-col gap-y-2 justify-center items-left bg-version object-cover xl:object-contain bg-no-repeat bg-center">
+      <div className="text-white h-screen px-4 xl:pl-24 xl:pr-56 flex flex-col gap-y-2 justify-center items-left bg-version md:object-cover xl:object-contain bg-no-repeat bg-center">
         <h1>{title}</h1>
         <p>{text}</p>
       </div>
